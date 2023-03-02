20e5ee336606c92049bed872b5609b7da9a8f9d9
preethamraj25dec <preethamredmi@gmail.com>
2023-03-02 09:35:31 +0530

    cahnge

--- Nike-Shoe-Error/Nike-Shoe-Error/Nike-Shoe-Store-master/src/components/Home/Home.js
+++ Nike-Shoe-Error/Nike-Shoe-Error/Nike-Shoe-Store-master/src/components/Home/Home.js
@@ -32,8 +32,8 @@ function Home() {
                     alt={shoe.name}
                     src={shoe.img}
                   />
-                  <h3 className="shoe-name">{shoe.name} </h3>
-                  <h3 className="shoe-price">${shoe.price} </h3>
+                  <h3 className="shoe-name">{shoe.name} j</h3>
+                  <h3 className="shoe-price">${shoe.price} s</h3>
                 </div>
               );
             })}
